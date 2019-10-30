18da817fdffc22995f106f3c54c5b98451ed5859
newkumpon <nontapap.th@gmail.com>
2019-10-30 17:00:47 +0700

    1st commit web service

---
+++ src/test/java/com/ktb/bc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.bc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
